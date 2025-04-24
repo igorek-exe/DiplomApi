b64a50e29e22c3e2806c7459dc3bc682ed590da2
igorek-exe <59085174+igorek-exe@users.noreply.github.com>
2025-04-24 11:21:02 +0300

    Update create.py

--- ToDoApi/create.py
+++ ToDoApi/create.py
@@ -8,7 +8,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 # User.objects.create_superuser('god', 'admin@example.com', '123')
-User.objects.create_user('user', 'user@example.com', 'aste11as')
-User.objects.create_user('user1', 'user@example.com', 'aste11as')
-User.objects.create_user('user2', 'user@example.com', 'aste11as')
+User.objects.create_user('user', 'user@example.com', 'asd123asd')
+User.objects.create_user('user1', 'user@example.com', 'asd123asd')
+User.objects.create_user('user2', 'user@example.com', 'asd123asd')
 print("пользователи успешно созданы.")
