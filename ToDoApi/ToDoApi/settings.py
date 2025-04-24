c3b9af60ede4b490a3e8182e7a58789c206982a5
igorek-exe <59085174+igorek-exe@users.noreply.github.com>
2025-04-24 11:18:32 +0300

    Update settings.py

--- ToDoApi/ToDoApi/settings.py
+++ ToDoApi/ToDoApi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$9jmugzbmh-i%a12@wb!f&346_4(#-d)5#y3+3zaox(ty(my3m'
+SECRET_KEY = 'secretkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
